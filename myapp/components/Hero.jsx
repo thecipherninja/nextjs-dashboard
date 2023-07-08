f042d345ab386656cbdd4d4b6b0037cff12bf673
thecipherninja <dassammy5@gmail.com>
2023-07-08 22:09:19 +0530

    initial commit

---
+++ myapp/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div id="main" class="flex flex-row"> 
+        <div class="bg-black w-588 h-screen">
+            <div class="w-246 h-88 bg-orange-400">
+                <p class="text-white font-family:Montserrat text-7xl not-italic font-bold leading-normal">
+                    Board.
+                </p>
+            </div>
+        </div>
+        <div class="bg-white w-852 h-screen">
+            
+        </div>
+    </div>
+    )
+}
+
+export default Hero
