404a2e4e988d927ea6bc2e76b7818bf99fdcbdb0
thecipherninja <dassammy5@gmail.com>
2023-07-10 22:35:21 +0530

    added redirects

--- myapp/next.config.js
+++ myapp/next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
-// require("dotenv").config
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  redirects: async() => {
+    return [
+      source: '/',
+      destination: '/signin',
+      permanent: true,
+    ],
+  }
 }
 
 module.exports = nextConfig
-
-// env: {
-//   GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-//   GOOGLE_CLIENT_SECRET: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-//   NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-//   JWT_SECRET: process.env.NEXT_PUBLIC_JWT_SECRET,
-// }
