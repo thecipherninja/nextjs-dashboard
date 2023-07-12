d16c48d831e3a2df16fae93d12c6564ac4178833
thecipherninja <dassammy5@gmail.com>
2023-07-12 17:35:32 +0530

    added randomness to line chart api data

--- myapp/pages/api/curvedata.js
+++ myapp/pages/api/curvedata.js
@@ -6,16 +6,23 @@ function generateGraphPoints() {
     const maxValue = 450; // Maximum value for the points
     const amplitude = (maxValue - minValue) / 2;
     const midpoint = (maxValue + minValue) / 2;
+    const randomnessFactor = 50; // Adjust this to control the randomness of the points
     const points = [];
   
     for (let i = 0; i < numPoints; i++) {
       const x = i; // Increment by 7 days
-      const y = amplitude * Math.sin((2 * Math.PI * i) / numPoints) + midpoint;
+      const y =
+        amplitude * Math.sin((2 * Math.PI * i) / numPoints) +
+        midpoint +
+        (Math.random() * randomnessFactor - randomnessFactor / 2);
       points.push({ x, y });
     }
   
     return points;
   }
+  
+  
+  
   
   // Example usage
 const graphPoints = generateGraphPoints();
--- myapp/pages/api/curvedata2.js
+++ myapp/pages/api/curvedata2.js
@@ -6,11 +6,15 @@ function generateGraphPoints2() {
     const maxValue = 350; // Maximum value for the points
     const amplitude = (maxValue - minValue) / 2;
     const midpoint = (maxValue + minValue) / 2;
+    const randomnessFactor = 50; // Adjust this to control the randomness of the points
     const points = [];
   
     for (let i = 0; i < numPoints; i++) {
       const x = i; // Increment by 7 days
-      const y = amplitude * Math.sin((2 * Math.PI * i) / numPoints) + midpoint;
+      const y =
+        amplitude * Math.sin((2 * Math.PI * i) / numPoints) +
+        midpoint +
+        (Math.random() * randomnessFactor - randomnessFactor / 2);
       points.push({ x, y });
     }
   
