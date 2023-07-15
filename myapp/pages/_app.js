0951e1f5cc4c9bdeb6fb8ec6e4ba627def6d44e6
thecipherninja <dassammy5@gmail.com>
2023-07-15 11:24:15 +0530

    added vercel analytics components

--- myapp/pages/_app.js
+++ myapp/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
 import {SessionProvider} from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   )
 }
